5407566adc8d43f90aa7e38fcf0c6e20224be2de
piyush571 <piyushsingh8412@gmail.com>
2025-11-04 15:33:57 +0530

    Add Student class with name and marks attributes

---
+++ exp-7,8,9/Student.java
@@ -0,0 +1,11 @@
+package experiment2_3.partB;
+
+class Student {
+    String name;
+    double marks;
+
+    Student(String name, double marks) {
+        this.name = name;
+        this.marks = marks;
+    }
+}
