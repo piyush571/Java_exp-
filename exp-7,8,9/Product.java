9c8dfeb7ec2148fbb3c0d6e3a0d1b93edcf5f591
piyush571 <piyushsingh8412@gmail.com>
2025-11-04 15:30:53 +0530

    Add Product class with properties and toString method

---
+++ exp-7,8,9/Product.java
@@ -0,0 +1,17 @@
+package experiment2_3.partC;
+
+class Product {
+    String name;
+    double price;
+    String category;
+
+    Product(String name, double price, String category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public String toString() {
+        return name + " | " + category + " | " + price;
+    }
+}
