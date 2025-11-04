0cf4cad6ef91b5ff0c2f4157a937e742c1c65226
piyush571 <piyushsingh8412@gmail.com>
2025-11-04 15:26:46 +0530

    Create Account class with basic structure

---
+++ exp-7,8,9/Account.java
@@ -0,0 +1,14 @@
+package experiment3_2.partC.com.bank;
+
+// File: Account.java
+
+
+public class Account {
+
+    private int accNo;
+    private String name;
+    private double balance;
+
+    public Account() {}
+    // getters/setters
+}
