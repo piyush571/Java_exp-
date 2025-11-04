cc371cff1de0f4df331ce9ab132a1d1e909cc6e8
piyush571 <piyushsingh8412@gmail.com>
2025-11-04 15:28:32 +0530

    Create Employee.java

---
+++ exp-7,8,9/Employee.java
@@ -0,0 +1,18 @@
+package experiment2_3.partA;
+
+class Employee {
+    String name;
+    int age;
+    double salary;
+
+    Employee(String name, int age, double salary) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return name + " | Age: " + age + " | Salary: " + salary;
+    }
+}
